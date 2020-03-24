f079c3fd7f62618fe8aaa45c73cab17e784bff2d
Vladik Belochkin <ilovesigna@gmail.com>
2020-03-24 13:55:30 +0200

    fis histiry

--- routes/web.js
+++ routes/web.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 
 	try {
 		myBalance = await vkcoin.api.getMyBalance()
-		transaction = await vkcoin.api.getTransactionList(2)
+		transaction = await vkcoin.api.getTransactionList(2).slice(0, 10)
 	}
 	catch (e) {
 		myBalance = 18_000_000
